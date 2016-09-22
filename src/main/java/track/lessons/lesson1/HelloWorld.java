2d9461aedb8359e10fdf1172448901379aab4ef6
DmitryArkhangelskiy <dmitry.arkhangelskiy@odnoklassniki.ru>
2016-09-22 20:32:14 +0300

    Revert "test commit"
    This reverts commit cf48ef691bac39953c051a3801211394740b9155.

--- src/main/java/track/lessons/lesson1/HelloWorld.java
+++ src/main/java/track/lessons/lesson1/HelloWorld.java
@@ -6,10 +6,6 @@ package track.lessons.lesson1;
 public class HelloWorld {
 
     public static void main(String[] args) {
-
-        if (args.length > 0) {
-            System.out.println(args[0]);
-        }
         System.out.println("Hello");
         try {
             System.out.println("");
