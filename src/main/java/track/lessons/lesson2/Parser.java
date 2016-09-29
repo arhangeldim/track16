8bdd92482e1418e46db901f0054f9477cb23c1ea
mif-a <fredegrec@gmail.com>
2016-09-29 21:40:39 +0300

    lesson2-task

--- src/main/java/track/lessons/lesson2/Parser.java
+++ src/main/java/track/lessons/lesson2/Parser.java
@@ -9,13 +9,25 @@ import java.io.FileReader;
 public class Parser {
 
     Document parse(String data) {
-        return null;
+        return new Document(data.split(" "));
     }
 
     public static void main(String[] args) throws Exception {
 
-        String path = "path/to/file";
+        String path = "D:\\new.txt";
         BufferedReader reader = new BufferedReader(new FileReader(path));
+
+        // reader умеет читать по строкам с помощью метода readLine()
+
+        // Создайте объект Parser
+        Parser parser = new Parser();
+        String s;
+        StringBuilder sb = new StringBuilder("");
+        while ((s = reader.readLine()) != null) {
+            sb.append(s);
+            sb.append(" ");
+        }
+        Document doc = parser.parse(sb.toString());
         // reader умеет читать по строкам с помощью метода readLine()
 
         // Создайте объект Parser
