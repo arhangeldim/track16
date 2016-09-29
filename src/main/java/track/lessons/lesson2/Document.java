5c528e71c25caf5a77bfc1c2ebc95f5abdd50d0e
Malik <gazizullin.malik2010@yandex.ru>
2016-09-29 23:25:13 +0300

    new

--- src/main/java/track/lessons/lesson2/Document.java
+++ src/main/java/track/lessons/lesson2/Document.java
@@ -11,10 +11,41 @@ public class Document {
     }
 
     int getTokenCount() {
-        return 0;
+        return (tokens == null) ? 0 : tokens.length;
     }
 
     boolean hasToken(String token) {
+        if (tokens == null) {
+            return false;
+        }
+        for (int i = 0; i < tokens.length; i++) {
+            if (token.equals(tokens[i])) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    public void add_text(String data) {
+        int countOfSpaces = 0;
+        for (int i = 0; i < data.length(); i++) {
+            if (data.charAt(i) == ' ') {
+                countOfSpaces++;
+            }
+        }
+        countOfSpaces++;
+        tokens = new String[countOfSpaces];
+
+        int indexToSearch = 0;
+        int curIndex = 0;
+
+        for (int i = 0; i < countOfSpaces - 1; i++) {
+            indexToSearch = data.indexOf(' ', indexToSearch);
+            tokens[i] = data.substring(curIndex, indexToSearch);
+            indexToSearch++;
+            curIndex = indexToSearch;
+        }
+
+        tokens[countOfSpaces - 1] = data.substring(curIndex);
     }
 }
