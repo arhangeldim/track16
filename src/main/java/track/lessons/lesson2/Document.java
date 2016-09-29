0effe3a048de5d06daeb9503ba4555e918303379
Leonid Kalichkin <leonid.kalichkin@phystech.edu>
2016-09-29 21:53:15 +0300

    implement Parser and Document methods

--- src/main/java/track/lessons/lesson2/Document.java
+++ src/main/java/track/lessons/lesson2/Document.java
@@ -6,15 +6,23 @@ package track.lessons.lesson2;
 public class Document {
     String[] tokens;
 
+    public Document(String[] tokens) {
+        this.tokens = tokens;
+    }
+
     String[] getTokens() {
-        return null;
+        return tokens;
     }
 
     int getTokenCount() {
-        return 0;
+        return tokens.length;
     }
 
     boolean hasToken(String token) {
+        for (String entry : tokens) {
+            if (entry.equals(token))
+                return true;
+        }
         return false;
     }
 }
