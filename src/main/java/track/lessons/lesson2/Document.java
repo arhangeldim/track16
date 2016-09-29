3b3fba5b7af687bea283465f82b9c4d00c2510f1
Leonid Shtanko <leonshting@gmail.com>
2016-09-29 21:27:07 +0300

    init commit

--- src/main/java/track/lessons/lesson2/Document.java
+++ src/main/java/track/lessons/lesson2/Document.java
@@ -4,17 +4,27 @@ package track.lessons.lesson2;
  *
  */
 public class Document {
+
+    Document(String[] tokens) {
+        this.tokens = tokens;
+    }
+
     String[] tokens;
 
     String[] getTokens() {
-        return null;
+        return tokens;
     }
 
     int getTokenCount() {
-        return 0;
+        return tokens.length;
     }
 
     boolean hasToken(String token) {
+        int i;
+        for (i = 0; i < this.tokens.length; i++) {
+            if (token.equals(tokens[i]))
+                return true;
+        }
         return false;
     }
 }
