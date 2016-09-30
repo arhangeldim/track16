4a6efbba1c1b373ec687c9af00b9a789e3e6cace
mrtsepa <tsepa.stas@gmail.com>
2016-09-30 18:04:32 +0300

    solution

--- src/main/java/track/lessons/lesson2/Parser.java
+++ src/main/java/track/lessons/lesson2/Parser.java
@@ -9,19 +9,28 @@ import java.io.FileReader;
 public class Parser {
 
     Document parse(String data) {
-        return null;
+        return new Document(data.split(" "));
     }
 
     public static void main(String[] args) throws Exception {
 
-        String path = "path/to/file";
+        String path = "/home/stas/Workspace/track-java/track16/src/main/resources/log4j.xml";
         BufferedReader reader = new BufferedReader(new FileReader(path));
         // reader умеет читать по строкам с помощью метода readLine()
 
+        StringBuilder data = new StringBuilder();
+
+        while (true) {
+            String line = reader.readLine();
+            if (line == null) {
+                break;
+            }
+            data.append(line);
+        }
         // Создайте объект Parser
-
+        Parser parser = new Parser();
         // Получите объект Document, реализовав метод parse()
-
-
+        Document document = parser.parse(data.toString());
+        System.out.println(document.getTokens()[0]);
     }
 }
