e55527275ac38a8805603ef4f1c6d9676bd0bcf1
lesina <lesha_lesovoy@mail.ru>
2016-09-30 11:47:05 +0300

    First probe of parser

--- src/main/java/track/lessons/lesson2/Parser.java
+++ src/main/java/track/lessons/lesson2/Parser.java
@@ -2,25 +2,34 @@ package track.lessons.lesson2;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-
+import java.util.Arrays;
 /**
  *
  */
 public class Parser {
 
     Document parse(String data) {
-        return null;
+        return new Document(data.split(" "));
     }
 
     public static void main(String[] args) throws Exception {
 
-        String path = "path/to/file";
+        String path = "src/main/java/track/lessons/lesson2/input.txt";
         BufferedReader reader = new BufferedReader(new FileReader(path));
         // reader умеет читать по строкам с помощью метода readLine()
+        StringBuilder text = new StringBuilder();
 
         // Создайте объект Parser
+        Parser parser = new Parser();
 
+        String line = reader.readLine();
+        while (line != null){
+            text.append(line);
+            line = reader.readLine();
+        }
         // Получите объект Document, реализовав метод parse()
+        Document document = parser.parse(text.toString());
+        System.out.println(Arrays.toString(document.getTokens()));
 
 
     }
