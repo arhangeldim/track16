20c522fb2844a0e5bfd399136d4562901d373a97
Alyona Egorova <ladyae@ya.ru>
2016-10-01 00:21:38 +0300

    solution

--- src/main/java/track/lessons/lesson2/Document.java
+++ src/main/java/track/lessons/lesson2/Document.java
@@ -4,17 +4,27 @@ package track.lessons.lesson2;
  *
  */
 public class Document {
+
+    Document(String[] tokens_) {
+        tokens = tokens_;
+    }
     String[] tokens;
 
     String[] getTokens() {
-        return null;
+        return tokens;
     }
 
     int getTokenCount() {
-        return 0;
+        return tokens.length;
     }
 
     boolean hasToken(String token) {
+
+        for (int i = 0; i < tokens.length; i++) {
+            if (token != null && tokens[i] == token){
+                return true;
+            }
+        }
         return false;
     }
 }
