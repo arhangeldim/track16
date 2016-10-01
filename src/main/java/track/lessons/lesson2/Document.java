37bb08d05d873c48cf2f907106427c86f349f72e
mif-a <fredegrec@gmail.com>
2016-10-01 22:47:16 +0300

    lesson2-doc

--- src/main/java/track/lessons/lesson2/Document.java
+++ src/main/java/track/lessons/lesson2/Document.java
@@ -19,9 +19,8 @@ public class Document {
     }
 
     boolean hasToken(String token) {
-        if (token == null) return false;
         for (int j = 0; j < tokens.length; j++) {
-            if (tokens[j].equals(token)){return true;}
+            if (token != null && tokens[j].equals(token)){return true;}
         }
         return false;
     }
