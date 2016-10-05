21d0354dfc0a597cceec755803138d8d79eb6ba0
George Komarov <geoolekom@gmail.com>
2016-10-05 08:41:57 +0300

    Modified classes to run

--- src/main/java/track/lessons/lesson2/Document.java
+++ src/main/java/track/lessons/lesson2/Document.java
@@ -1,20 +1,37 @@
 package track.lessons.lesson2;
-
 /**
  *
  */
+
 public class Document {
     String[] tokens;
 
+    Document(String data) {
+        tokens = data.split(" ");
+    }
+
     String[] getTokens() {
-        return null;
+        String[] strings = new String[this.getTokenCount()];
+        int j = 0;
+        for (String iterator : tokens) {
+            strings[j] = iterator;
+            j++;
+        }
+        return strings;
     }
 
     int getTokenCount() {
-        return 0;
+        int counter = 0;
+        for (String iterator : tokens) {
+            counter ++;
+        }
+        return counter;
     }
 
     boolean hasToken(String token) {
+        for (String iterator : tokens) {
+            if (token.equals(iterator)) return true;
+        }
         return false;
     }
 }
