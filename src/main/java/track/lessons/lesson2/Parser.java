92ae29d1f8290c7cbd65d01f8afde7f4158b7345
andpago <andpago@gmail.com>
2016-10-06 11:07:18 +0300

    Homework1

--- src/main/java/track/lessons/lesson2/Parser.java
+++ src/main/java/track/lessons/lesson2/Parser.java
@@ -9,19 +9,26 @@ import java.io.FileReader;
 public class Parser {
 
     Document parse(String data) {
-        return null;
+        String[] tokens = data.split(' ');
+        Document doc = new Document(tokens);
+        return doc;
     }
 
     public static void main(String[] args) throws Exception {
 
-        String path = "path/to/file";
+        String path = "/home/andpago/Desktop/to_parse.txt";
         BufferedReader reader = new BufferedReader(new FileReader(path));
         // reader умеет читать по строкам с помощью метода readLine()
-
+        StringBuilder builder = new StringBuilder();
+        String line = "";
+        while (line != null) {
+            builder.append(line);
+            line = reader.readLine();
+        }
         // Создайте объект Parser
-
+        Parser parser = new Parser();
         // Получите объект Document, реализовав метод parse()
-
+        Document doc = new parser.parse(builder.toString());
 
     }
 }
