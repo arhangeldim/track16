d59fc15277a98f0cd04dc02cb04d80e9321a9d72
carakanz <carakanz@gmail.com>
2016-10-06 19:33:49 +0300

    test4

--- src/main/java/track/lessons/lesson2/Parser.java
+++ src/main/java/track/lessons/lesson2/Parser.java
@@ -16,16 +16,14 @@ public class Parser {
     }
 
     public static void main(String[] args) throws Exception {
-
         String path = "src\\main\\java\\track\\inputFile\\input.txt";
         BufferedReader reader = new BufferedReader(new FileReader(path)) ;
-            // reader умеет читать по строкам с помощью метода readLine()
-            String readSrting=reader.readLine();
-            // Создайте объект Parser
-            Parser parser = new Parser();
-            Document document = parser.parse (readSrting);
-             // Получите объект Document, реализовав метод parse()
-
+        // reader умеет читать по строкам с помощью метода readLine()
+        String readSrting = reader.readLine();
+        // Создайте объект Parser
+        Parser parser = new Parser();
+        Document document = parser.parse( readSrting);
+        // Получите объект Document, реализовав метод parse()
     }
 
 }
