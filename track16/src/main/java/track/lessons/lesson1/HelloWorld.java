be4849454dcd3af0aeca01e993f76b904d5b5546
George Komarov <geoolekom@gmail.com>
2016-10-06 18:55:17 +0300

    New lesson

---
+++ track16/src/main/java/track/lessons/lesson1/HelloWorld.java
@@ -0,0 +1,14 @@
+package track.lessons.lesson1;
+
+/**
+ *
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("arg[" + i + "]=" + args[i]);
+        }
+    }
+}
---
+++ track16/src/main/java/track/lessons/lesson2/Document.java
@@ -0,0 +1,20 @@
+package track.lessons.lesson2;
+
+/**
+ *
+ */
+public class Document {
+    String[] tokens;
+
+    String[] getTokens() {
+        return null;
+    }
+
+    int getTokenCount() {
+        return 0;
+    }
+
+    boolean hasToken(String token) {
+        return false;
+    }
+}
