6dc5e4e3a78adeada422b29cb2dcf3fa61f62576
Babin Oleg <babinoleg@mail.ru>
2016-10-08 15:36:59 +0300

    check system conflicts with previous task

--- src/main/java/track/lessons/lesson2/Parser.java
+++ src/main/java/track/lessons/lesson2/Parser.java
@@ -9,19 +9,34 @@ import java.io.FileReader;
 public class Parser {
 
     Document parse(String data) {
-        return null;
+        Document document = new Document();
+        document.tokens = data.split(" ");
+        return document;
     }
 
     public static void main(String[] args) throws Exception {
 
-        String path = "path/to/file";
+        String path = "/home/oleg/input.txt";
         BufferedReader reader = new BufferedReader(new FileReader(path));
         // reader умеет читать по строкам с помощью метода readLine()
-
+        StringBuilder stringBuilder = new StringBuilder();
+        while (true) {
+            String line = reader.readLine();
+            if (line == null) {
+                break;
+            }
+            stringBuilder.append(line);
+            stringBuilder.append(" ");
+        }
+        String result = stringBuilder.toString();
         // Создайте объект Parser
+        Parser parser = new Parser();
 
         // Получите объект Document, реализовав метод parse()
-
+        Document myDoc = parser.parse(result);
+        for (String tok: myDoc.tokens) {
+            System.out.println(tok);
+        }
 
     }
 }
