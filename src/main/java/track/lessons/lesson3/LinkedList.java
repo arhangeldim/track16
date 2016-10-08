86123a6343be3a184e415ee4e1c0a461aea69a9e
Malik <gazizullin.malik2010@yandex.ru>
2016-10-09 01:55:09 +0300

    a

--- src/main/java/track/lessons/lesson3/LinkedList.java
+++ src/main/java/track/lessons/lesson3/LinkedList.java
@@ -112,34 +112,3 @@ public class LinkedList extends List implements Stack, Queue {
     }
 }
 
-class Node {
-    private Node next;
-    private Node prev;
-    private int value;
-
-    public Node(int value, Node prev, Node next) {
-        this.value = value;
-        this.prev = prev;
-        this.next = next;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public Node getPrev() {
-        return prev;
-    }
-
-    public void changeNext(Node next) {
-        this.next = next;
-    }
-
-    public void changePrev(Node prev) {
-        this.prev = prev;
-    }
-}
