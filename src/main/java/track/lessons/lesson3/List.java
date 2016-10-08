614e6ad7a207de359867bac47fa7ac60872c5be2
Malik <gazizullin.malik2010@yandex.ru>
2016-10-09 02:14:00 +0300

    new

--- src/main/java/track/lessons/lesson3/List.java
+++ src/main/java/track/lessons/lesson3/List.java
@@ -5,11 +5,11 @@ package track.lessons.lesson3;
  *
  */
 public abstract class List {
-    abstract public void add(int item); //- добавить элемент в конец списка
+    abstract void add(int item); //- добавить элемент в конец списка
 
-    abstract public int remove(int idx); //- удалить элемент по индексу idx, если idx некорректный напечатать ошибку,
+    abstract int remove(int idx); //- удалить элемент по индексу idx, если idx некорректный напечатать ошибку,
 
-    abstract public int get(int idx); //- получить элемент по индексу
+    abstract int get(int idx); //- получить элемент по индексу
 
-    abstract public int size(); //- сколько элементов в данный момент в списке
+    abstract int size(); //- сколько элементов в данный момент в списке
 }
