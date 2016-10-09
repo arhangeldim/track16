c3d9ae7bb1d35877c5aaa9993b69752e42e66c68
mrtsepa <tsepa.stas@gmail.com>
2016-10-09 16:35:17 +0300

    solution

--- src/main/java/track/lessons/lesson3/DynamicList.java
+++ src/main/java/track/lessons/lesson3/DynamicList.java
@@ -3,5 +3,39 @@ package track.lessons.lesson3;
 /**
  *
  */
-public class DynamicList {
+public class DynamicList extends List {
+
+    private int[] array;
+    private int capacity;
+
+    public DynamicList() {
+        capacity = 1;
+        array = new int[capacity];
+    }
+
+    @Override
+    public void add(int item) {
+        if (size >= capacity) {
+            capacity *= 2;
+            int[] tmpArray = new int[capacity];
+            System.arraycopy(array, 0, tmpArray, 0, size);
+            array = tmpArray;
+        }
+        array[size] = item;
+        size++;
+        System.out.println(capacity);
+    }
+
+    @Override
+    public int remove(int idx) {
+        int elem = array[idx];
+        System.arraycopy(array, idx + 1, array, idx, size - idx - 1);
+        size--;
+        return elem;
+    }
+
+    @Override
+    public int get(int idx) {
+        return array[idx];
+    }
 }
--- src/main/java/track/lessons/lesson3/ListMain.java
+++ src/main/java/track/lessons/lesson3/ListMain.java
@@ -8,11 +8,10 @@ public class ListMain {
     public static void main(String[] args) {
 
 
-//        List list = new DynamicList();
-//        list.add(1);
-//        list.add(2);
-//        list.add(10);
-//        int first = list.remove(0);
-
+        List list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(10);
+        int first = list.remove(0);
     }
 }
