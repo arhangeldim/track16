2e359d37731acf9196960bfb3247617f8c340ef5
Malik <gazizullin.malik2010@yandex.ru>
2016-10-09 15:25:10 +0300

    new_pull

---
+++ track16/src/main/java/track/lessons/lesson3/DynamicList.java
@@ -0,0 +1,58 @@
+package track.lessons.lesson3;
+
+/**
+ *
+ */
+public class DynamicList extends List {
+
+    private int[] dynamicArray;
+    private int size;
+    private int buffer;
+
+    @Override
+    public void add(int item) {
+        if (buffer >= size + 1) {
+            dynamicArray[size] = item;
+            size++;
+        } else {
+            increaseBuffer();
+            add(item);
+        }
+    }
+
+    private void increaseBuffer() {
+        if (buffer == 0) {
+            buffer = 2;
+            dynamicArray = new int[buffer];
+        } else {
+            buffer = buffer * 2;
+            int[] newArray = new int[buffer];
+            System.arraycopy(dynamicArray, 0, newArray, 0, size);
+            dynamicArray = newArray;
+        }
+    }
+
+    @Override
+    public int remove(int idx) {
+        if (size < 1 || idx < 0) {
+            return -1;
+        }
+        int temp = dynamicArray[idx];
+        System.arraycopy(dynamicArray, idx + 1, dynamicArray, idx, size - idx);
+        size--;
+        return temp;
+    }
+
+    @Override
+    public int get(int idx) {
+        if (idx + 1 > size) {
+            return -1;
+        }
+        return dynamicArray[idx];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+}
