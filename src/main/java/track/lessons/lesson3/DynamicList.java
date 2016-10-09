0d5aa2f9732557791dd0ee3d658ce27eb88fe0c9
kitnoel <kitnoelka@yandex.ru>
2016-10-09 22:11:57 +0300

    List

--- src/main/java/track/lessons/lesson3/DynamicList.java
+++ src/main/java/track/lessons/lesson3/DynamicList.java
@@ -1,7 +1,61 @@
 package track.lessons.lesson3;
 
+
 /**
  *
  */
-public class DynamicList {
+public class DynamicList extends List {
+    private int[] items;
+
+    DynamicList() {
+        items = new int[0];
+    }
+
+    /**
+     * добавить элемент в конец списка
+     */
+    @Override
+    void add(int item) {
+        int[] newItems = new int[items.length + 1];
+        System.arraycopy(items, 0, newItems, 0, items.length);
+        newItems[items.length] = item;
+        items = newItems;
+    }
+
+    /**
+     * удалить элемент по индексу idx, если idx некорректный напечатать ошибку, если ок - вернуть удаленный элемент
+     */
+    @Override
+    int remove(int idx) {
+        if (idx < items.length) {
+            int[] newItems = new int[items.length - 1];
+            System.arraycopy(items, 0, newItems, 0, idx);
+            System.arraycopy(items, idx + 1, newItems, 0, items.length - 1 - idx);
+            int removedElem = items[idx];
+            items = newItems;
+            return removedElem;
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    /**
+     * получить элемент по индексу
+     */
+    @Override
+    int get(int idx) {
+        if (idx < items.length) {
+            return items[idx];
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    /**
+     * сколько элементов в данный момент в списке
+     */
+    @Override
+    int size() {
+        return items.length;
+    }
 }
--- src/main/java/track/lessons/lesson3/LinkedList.java
+++ src/main/java/track/lessons/lesson3/LinkedList.java
@@ -1,7 +1,116 @@
 package track.lessons.lesson3;
 
+import java.util.EmptyStackException;
+
 /**
  *
  */
-public class LinkedList {
+public class LinkedList extends List implements Stack, Queue {
+    private class Node {
+        Node next;
+        Node prev;
+        final int value;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    LinkedList() {
+        size = 0;
+    }
+
+    @Override
+    void add(int item) {
+        size++;
+        Node newNode = new Node(item);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node cur = head;
+        while (cur.next != null) {
+            cur = cur.next;
+        }
+        newNode.prev = cur;
+        cur.next = newNode;
+
+    }
+
+    @Override
+    int remove(int idx) {
+        if (idx >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node cur = head;
+        for (int i = 0; i < idx; i++) {
+            cur = cur.next;
+        }
+        if (cur == head) {
+            head = cur.next;
+        } else {
+            cur.prev.next = cur.next;
+        }
+        if (cur.next != null) {
+            cur.next.prev = cur.prev;
+        }
+        size--;
+        return cur.value;
+    }
+
+    @Override
+    int get(int idx) {
+        if (idx >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node cur = head;
+        for (int i = 0; i < idx; i++) {
+            cur = cur.next;
+        }
+        return cur.value;
+    }
+
+    @Override
+    int size() {
+        return size;
+    }
+
+    @Override
+    public void enqueue(int value) {
+        add(value);
+    }
+
+    @Override
+    public int dequeue() {
+        Node first = head;
+        if (first == null) {
+            throw new EmptyStackException();
+        }
+        head = head.next;
+        size--;
+        return first.value;
+    }
+
+    @Override
+    public void push(int value) {
+        size++;
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    @Override
+    public int pop() {
+        return this.dequeue();
+    }
+
+
 }
