f06b39578e9579084a9fcb5676fcb07b006db018
Rinat Aynulin <rinataynulin@gmail.com>
2016-10-11 15:59:54 +0300

    add DynamicList, LinkedList and tests for them

--- src/main/java/track/lessons/lesson3/DynamicList.java
+++ src/main/java/track/lessons/lesson3/DynamicList.java
@@ -1,7 +1,81 @@
 package track.lessons.lesson3;
 
+import java.util.Arrays;
+
 /**
  *
  */
-public class DynamicList {
+public class DynamicList extends List {
+
+    private static final int DEFAULT_CAPACITY = 10;
+    private int[] elements;
+    private int size;
+
+    public DynamicList() {
+        elements = new int[DEFAULT_CAPACITY];
+    }
+
+    public DynamicList(int capacity) {
+        if (capacity >= 0) {
+            elements = new int[capacity];
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public void add(int item) {
+        if (size == elements.length) {
+            resize(2 * elements.length);
+        }
+        elements[size++] = item;
+    }
+
+    private void resize(int capacity) {
+        int[] copy = new int[capacity];
+        System.arraycopy(elements, 0, copy, 0, size);
+        elements = copy;
+    }
+
+    @Override
+    public int remove(int idx) {
+        if (idx < 0 || idx >= size) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+
+        int item = elements[idx];
+        System.arraycopy(elements, idx + 1, elements, idx, size-- - idx);
+        if (size > 0 && size == elements.length / 4) {
+            resize(elements.length / 2);
+        }
+        return item;
+    }
+
+    @Override
+    public int get(int idx) {
+        if (idx < 0 || idx >= size) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+        return elements[idx];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    public int capacity() {
+        return elements.length;
+    }
+
+    @Override
+    public String toString() {
+        int[] elems = new int[size];
+        System.arraycopy(elements, 0, elems, 0, size);
+        return "DynamicList{" +
+                "elements=" + Arrays.toString(elems) +
+                ", size=" + size +
+                ", capacity=" + elements.length +
+                '}';
+    }
 }
---
+++ src/main/java/track/lessons/lesson3/Stack.java
@@ -0,0 +1,9 @@
+package track.lessons.lesson3;
+
+/**
+ * Created by Aynulin on 11.10.2016.
+ */
+public interface Stack {
+    void push(int value);
+    int pop();
+}
