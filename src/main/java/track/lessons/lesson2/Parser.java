b05d4247c803a880c33b4205f9e38e41711d5712
Andrey Garkavyy <andrewgarkavyy@gmail.com>
2016-10-11 20:44:51 +0300

    too many imports

--- src/main/java/track/lessons/lesson2/Parser.java
+++ src/main/java/track/lessons/lesson2/Parser.java
@@ -2,9 +2,6 @@ package track.lessons.lesson2;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.InputStreamReader;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  *
