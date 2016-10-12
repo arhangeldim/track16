610c76373b6bf08f07989c11b0c8889588e69a61
egiby <egiby@egiby>
2016-10-12 22:40:22 +0300

    style fix

--- src/main/java/track/lessons/lesson3/ListMain.java
+++ src/main/java/track/lessons/lesson3/ListMain.java
@@ -7,12 +7,17 @@ public class ListMain {
 
     public static void main(String[] args) {
         Queue queue = new LinkedList();
+        Stack stack = new LinkedList();
         for (int i = 0; i < 10; ++i) {
             queue.enqueue(i);
+            stack.push(i);
         }
 
         for (int i = 0; i < 10; ++i) {
+            System.out.print("queue: ");
             System.out.println(queue.dequeue());
+            System.out.print("stack: ");
+            System.out.println(stack.pop());
         }
 
         List list = new DynamicList();
@@ -21,6 +26,5 @@ public class ListMain {
         list.add(10);
         int first = list.remove(0);
         System.out.println(first);
-        // System.out.println();
     }
 }
--- src/main/java/track/lessons/lesson3/Stack.java
+++ src/main/java/track/lessons/lesson3/Stack.java
@@ -5,5 +5,6 @@ package track.lessons.lesson3;
  */
 public interface Stack {
     void push(int value);
+
     int pop();
 }
