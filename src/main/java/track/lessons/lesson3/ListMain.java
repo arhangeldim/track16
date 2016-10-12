584b16c695514e45db7e16691026e3320314bae3
Alexandra <aiglina@gmail.com>
2016-10-12 14:08:24 +0300

    task list added

--- src/main/java/track/lessons/lesson3/ListMain.java
+++ src/main/java/track/lessons/lesson3/ListMain.java
@@ -8,11 +8,14 @@ public class ListMain {
     public static void main(String[] args) {
 
 
-//        List list = new DynamicList();
-//        list.add(1);
-//        list.add(2);
-//        list.add(10);
-//        int first = list.remove(0);
+        List list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(10);
+        int first = list.remove(0);
+        System.out.println(first);
+        System.out.println(list.size());
+        System.out.println(list.get(0));
 
     }
 }
