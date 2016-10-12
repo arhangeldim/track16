c1820fe82e4797fa1f0d9fedd8ad6f785d6cd330
zerts <zertsalov96@yandex.ru>
2016-10-12 03:23:15 +0300

    Task 1

--- src/main/java/track/lessons/lesson2/Document.java
+++ src/main/java/track/lessons/lesson2/Document.java
@@ -1,19 +1,26 @@
 package track.lessons.lesson2;
+
 import java.security.PublicKey;
 /**
  *
  */
+
 public class Document {
+
     public Document(String[] data) {
         tokens = data;
     }
+
     private String[] tokens;
+
     String[] getTokens() {
         return tokens;
     }
+
     int getTokenCount() {
         return tokens.length;
     }
+
     boolean hasToken(String token) {
         for (String s : tokens) {
             if (s.equals(token)) {
