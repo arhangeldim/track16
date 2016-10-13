d5270c679123e1f6e3f0ccce096f7e22c8bacd7a
alexparub <alex130396@mail.ru>
2016-10-13 23:23:57 +0300

    Added list classes

--- src/main/java/track/lessons/lesson3/List.java
+++ src/main/java/track/lessons/lesson3/List.java
@@ -3,5 +3,27 @@ package track.lessons.lesson3;
 /**
  *
  */
-public class List {
+public abstract class List {
+    int size;
+    // добавить элемент в конец списка
+    abstract void add(int item);
+    // удалить элемент по индексу idx, если idx некорректный напечатать ошибку, если ок - вернуть удаленный элемент
+    abstract int remove(int idx);
+    // получить элемент по индексу
+    abstract int get(int idx);
+    //распечать список
+    abstract void print();
+    // сколько элементов в данный момент в списке
+    int size()
+    {
+        return size;
+    }
+    boolean isValid(int idx)
+    {
+        if(idx < 0 || idx >= size)
+        {
+            throw new IndexOutOfBoundsException("Error: Index is unavailable");
+        }
+        return true;
+    }
 }
--- src/main/java/track/lessons/lesson3/ListMain.java
+++ src/main/java/track/lessons/lesson3/ListMain.java
@@ -8,11 +8,55 @@ public class ListMain {
     public static void main(String[] args) {
 
 
-//        List list = new DynamicList();
-//        list.add(1);
-//        list.add(2);
-//        list.add(10);
-//        int first = list.remove(0);
+        List list = new DynamicList(5);
+        list.add(1);
+        list.add(2);
+        list.add(10);
+        list.print();
+        int first = list.remove(0);
+        int second = list.get(1);
+        System.out.println(list.size());
+        list.print();
+        list.remove(1);
+        list.remove(0);
+        list.print();
+        for(int i = 0; i < 25; i++)
+        {
+            list.add(i);
+        }
+        list.print();
 
+        List list1 = new LinkedList();
+        list1.add(5);
+        list1.add(8);
+        list1.add(13);
+        System.out.println(list1.size());
+        list1.print();
+        System.out.println(list1.get(2));
+        list1.remove(1);
+        list1.print();
+        list1.remove(1);
+        list1.print();
+        list1.remove(0);
+        list1.print();
+        System.out.println(list1.size());
+
+        LinkedList list2 = new LinkedList();
+        list2.push(5);
+        list2.push(4);
+        System.out.println(list2.pop());
+        list2.print();
+        System.out.println("\n");
+        LinkedList list3 = new LinkedList();
+        list3.enqueue(67);
+        list3.enqueue(23);
+        list3.print();
+        System.out.println(list3.dequeue());
+        System.out.println("\n");
+        list3.print();
+        /*list1.add(13);
+        System.out.println(list1.size());
+
+        System.out.println(list1.get(0));*/
     }
 }
