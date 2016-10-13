717a3fdcd709a2fcdea2729b31b15867e27b0e35
DmitryArkhangelskiy <dmitry.arkhangelskiy@odnoklassniki.ru>
2016-10-13 12:00:48 +0300

    Container task

---
+++ src/main/java/track/container/config/ConfigReader.java
@@ -0,0 +1,31 @@
+package track.container.config;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ *
+ */
+public interface ConfigReader {
+
+    /*
+    Все переменные, объявленные в интерфейсе по-умолчанию константы
+     */
+    String TAG_BEANS = "beans";
+    String TAG_PROPERTIES = "properties";
+    String ATTR_NAME = "name";
+    String ATTR_VALUE = "val";
+    String ATTR_REF = "ref";
+    String ATTR_BEAN_ID = "id";
+    String ATTR_BEAN_CLASS = "class";
+
+    /**
+     * Для методов интерфейса стоит писать документацию
+     *
+     *
+     * @param pathToFile - Файл конфигурации (Сигнатуру не менять, используется в тестах)
+     * @return - список бинов
+     */
+    List<Bean> parseBeans(File pathToFile) throws InvalidConfigurationException;
+
+}
