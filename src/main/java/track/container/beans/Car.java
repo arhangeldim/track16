edc269333578b077427103ec693bdcc43f10cc0d
Frystile <echelon2@mail.ru>
2016-10-13 18:10:57 +0300

    fails

--- src/main/java/track/container/beans/Car.java
+++ src/main/java/track/container/beans/Car.java
@@ -39,8 +39,12 @@ public class Car {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
         Car car = (Car) obj;
         return Objects.equals(gear, car.gear) &&
                 Objects.equals(engine, car.engine);
