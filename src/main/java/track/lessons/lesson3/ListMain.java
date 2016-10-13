b2dcaaf3904d709e4479deddd1746dff9e0e287c
zerts <zertsalov96@yandex.ru>
2016-10-14 00:59:39 +0300

    Task 2

--- src/main/java/track/lessons/lesson3/ListMain.java
+++ src/main/java/track/lessons/lesson3/ListMain.java
@@ -7,12 +7,12 @@ public class ListMain {
 
     public static void main(String[] args) {
 
-
 //        List list = new DynamicList();
 //        list.add(1);
 //        list.add(2);
 //        list.add(10);
 //        int first = list.remove(0);
+
 
     }
 }
