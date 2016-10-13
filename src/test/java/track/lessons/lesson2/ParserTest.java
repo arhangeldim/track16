455e76d91350a7fca7b4119ebe8a7d02a394812c
Sergey Ivanchev <sergeyivanychev@gmail.com>
2016-10-14 01:45:45 +0300

    Completed tests for parser

--- src/test/java/track/lessons/lesson2/ParserTest.java
+++ src/test/java/track/lessons/lesson2/ParserTest.java
@@ -4,11 +4,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import javax.print.Doc;
 
 /**
  *  lipsum 69
@@ -43,14 +46,12 @@ public class ParserTest {
 
     @Test
     public void parse() throws Exception {
-        Assert.assertTrue(data != null && data.length() > 0);
-        Parser parser = new Parser();
-        Document doc = parser.parse(data);
-        Assert.assertEquals(218, doc.getTokenCount());
-
-        System.out.println(doc.getTokens());
-
-        Assert.assertTrue(doc.hasToken("</layout>"));
+        Iterator it = tests.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            Parser parser = new Parser();
+            Document doc = parser.parse((String)pair.getKey());
+            Assert.assertEquals((int)pair.getValue(), doc.getTokenCount());
+        }
     }
-
 }
