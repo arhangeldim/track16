52736b021fa8170ca59692e6ec56b4e2a69214eb
Лопаткин Даниил <lopatkindaniil@MBP-Lopatkin.campus.mipt.ru>
2016-10-13 16:46:22 +0300

    List homework.

--- src/main/java/track/lessons/lesson3/DynamicList.java
+++ src/main/java/track/lessons/lesson3/DynamicList.java
@@ -3,5 +3,49 @@ package track.lessons.lesson3;
 /**
  *
  */
-public class DynamicList {
+public class DynamicList extends List {
+    int[] content;
+
+    public DynamicList() {
+        content = null;
+    }
+
+    void add(int item) {
+        if (content == null) {
+            content = new int[1];
+            content[0] = item;
+        } else {
+            int[] newContent = new int[content.length + 1];
+            System.arraycopy(content, 0, newContent, 0, content.length);
+            content = newContent;
+            content[content.length - 1] = item;
+        }
+    }
+
+    int remove(int idx) {
+        int[] newContent = new int[content.length - 1];
+        if (idx >= content.length ) {
+            System.out.println("This list doesn't have element with this index.");
+            return -1;
+        }
+        System.arraycopy(content, 0, newContent, 0, idx);
+        if (idx < content.length - 1) {
+            System.arraycopy(content, idx + 1 , newContent, idx, content.length - idx - 1);
+        }
+        int value = content[idx];
+        content = newContent;
+        return value;
+    }
+
+    int get(int idx) {
+        if (idx >= content.length ) {
+            System.out.println("This list doesn't have element with this index.");
+            return -1;
+        }
+        return content[idx];
+    }
+
+    int size() {
+        return content.length;
+    }
 }
--- src/main/java/track/lessons/lesson3/LinkedList.java
+++ src/main/java/track/lessons/lesson3/LinkedList.java
@@ -3,5 +3,119 @@ package track.lessons.lesson3;
 /**
  *
  */
-public class LinkedList {
+public class LinkedList extends List implements Queue, Stack {
+    private class Node {
+        private Node next;
+        private Node prev;
+        private int value;
+
+        public Node(Node prev, int value) {
+            next = null;
+            this.prev = prev;
+            this.value = value;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public void setNext(int value) {
+            next = new Node(this, value);
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getLast() {
+            if (this.next == null) {
+                return this;
+            } else {
+                return this.next.getLast();
+            }
+        }
+
+        public Node getN(int number) {
+            if (number == 0) {
+                return this;
+            } else if (this.next == null) {
+                return null;
+            } else {
+                return this.next.getN(number - 1);
+            }
+        }
+
+        public int size() {
+            if (next == null) {
+                return 1;
+            } else {
+                return 1 + next.size();
+            }
+        }
+    }
+
+    Node content;
+
+    public LinkedList() {
+        content = null;
+    }
+
+    void add(int item) {
+        if (content == null) {
+            content = new Node(null, item);
+        } else {
+            content.getLast().setNext(item);
+        }
+    }
+
+    int remove(int idx) {
+        Node numberN = content.getN(idx);
+        if (numberN == null) {
+            System.out.println("This list doesn't have element with this index.");
+            return 0;
+        }
+        if (numberN.prev != null) {
+            numberN.prev.setNext(numberN.next);
+        }
+        if (numberN.next != null) {
+            numberN.next.setPrev(numberN.prev);
+        }
+        if (idx == 0) {
+            content = content.next;
+        }
+        return numberN.value;
+    }
+
+    int get(int idx) {
+        Node numberN = content.getN(idx);
+        if (numberN == null) {
+            System.out.println("This list doesn't have element with this index.");
+            return 0;
+        }
+        return numberN.getValue();
+    }
+
+    int size() {
+        return content.size();
+    }
+
+    public void enqueue(int value) {
+        this.add(value);
+    }
+
+    public int dequeu() {
+        return this.remove(0);
+    }
+
+    public void push(int value) {
+        this.add(value);
+    }
+
+    public int pop() {
+        return this.remove(this.size() - 1);
+    }
 }
