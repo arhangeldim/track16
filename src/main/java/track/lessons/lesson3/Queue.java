40f0c0533cc1def92275a1f6e79ca283f5f9525e
zerts <zertsalov96@yandex.ru>
2016-10-15 13:49:50 +0300

    SecondTask

---
+++ src/main/java/track/lessons/lesson3/Queue.java
@@ -0,0 +1,7 @@
+package track.lessons.lesson3;
+
+/**
+ * Created by zerts on 15.10.16.
+ */
+public class Queue {
+}
---
+++ src/test/java/track/lessons/lesson3/DynamicListTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by zerts on 15.10.16.
+ */
+public class DynamicListTest {
+
+}
