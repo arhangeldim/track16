b21843f28a6b49bbcf1d1c7a142cfc392e202c5d
ivan <nagaiko.ivan@gmail.com>
2016-10-15 18:38:57 +0300

    delete errors

--- src/main/java/track/lessons/lesson3/List.java
+++ src/main/java/track/lessons/lesson3/List.java
@@ -3,13 +3,9 @@ package track.lessons.lesson3;
 /**
  *
  */
-<<<<<<< HEAD
 public abstract class List {
-        abstract void add(int item);
-        abstract int remove(int idx);
-        abstract int get(int idx);
-        abstract int size();
-=======
-public class List {
->>>>>>> upstream/master
+    abstract void add(int item);
+    abstract int remove(int idx);
+    abstract int get(int idx);
+    abstract int size();
 }
