eb0c11083393ca0e3b5a8c34ae42daa8e5362f05
MrTsepa <tsepa.stas@gmail.com>
2016-10-16 02:41:05 +0300

    solution

--- src/main/java/track/container/Main.java
+++ src/main/java/track/container/Main.java
@@ -1,18 +1,24 @@
 package track.container;
 
+import track.container.beans.Car;
+import track.container.beans.Engine;
+import track.container.beans.Gear;
 import track.container.config.Bean;
 import track.container.config.ConfigReader;
 import track.container.config.InvalidConfigurationException;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.util.Objects;
 
 /**
  *
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
 
         ConfigReader configReader = new JsonConfigReader();
         List<Bean> beans;
@@ -24,8 +30,13 @@ public class Main {
             return;
         }
         for (Bean bean : beans)
-        System.out.println(bean);
-
+            System.out.println(bean);
+        Container container = new Container(beans);
+        Engine engine = (Engine) container.getById("engineBean");
+        Car car = (Car) container.getByClass("track.container.beans.Car");
+        Field field = Gear.class.getDeclaredField("count");
+        field.setAccessible(true);
+        System.out.println(Integer.TYPE.getName());
         /*
 
         ПРИМЕР ИСПОЛЬЗОВАНИЯ
