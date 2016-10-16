a04ac5a8806350f244bd9e68d5793b97c6e5d3ee
Kirill Korolev <kirill.korolef@gmail.com>
2016-10-16 14:56:30 +0300

    Implemented List classes

--- src/main/java/track/lessons/lesson3/DynamicList.java
+++ src/main/java/track/lessons/lesson3/DynamicList.java
@@ -3,5 +3,51 @@ package track.lessons.lesson3;
 /**
  *
  */
-public class DynamicList {
+public class DynamicList extends List {
+    private static final int defaultCap = 10;
+    private int [] data;
+
+    public DynamicList() {
+        data = new int[defaultCap];
+        size = 0;
+    }
+
+    public DynamicList(int initValue) {
+        data = new int[defaultCap];
+        size = 1;
+        data[0] = initValue;
+    }
+
+    private void extend() {
+        int [] newData = new int[data.length * 2];
+        System.arraycopy(data, 0, newData, 0, data.length);
+        data = newData;
+    }
+
+    public void add(int value) {
+        if (size == data.length) {
+            extend();
+        }
+        data[size] = value;
+        size++;
+    }
+
+    public int remove(int index) {
+        if (size == 0 || (index < 0 && index >= size - 1)) {
+            System.out.println("bad index");
+            return -1;
+        }
+        int deleted = data[index];
+        System.arraycopy(data, index + 1, data, index, data.length - index - 1);
+        size--;
+        return deleted;
+    }
+
+    public int get(int index) {
+        if (size == 0 || (index < 0 && index >= size - 1)) {
+            System.out.println("bad index");
+            return -1;
+        }
+        return data[index];
+    }
 }
--- src/main/java/track/lessons/lesson3/LinkedList.java
+++ src/main/java/track/lessons/lesson3/LinkedList.java
@@ -3,5 +3,66 @@ package track.lessons.lesson3;
 /**
  *
  */
-public class LinkedList {
+
+public class LinkedList extends List {
+    private Node head; // [head] <-> [index = 0] <-> [index = 1] <-> ... <-> [tail]
+    private Node tail; // [Node]     [  Node   ]     [  Node   ]     ...     [Node]
+
+    public LinkedList() {
+        head = new Node(0);
+        tail = new Node(0);
+        size = 0;
+    }
+
+    public LinkedList(int initValue) {
+        head = new Node(0);
+        tail = new Node(0);
+        Node newNode = new Node(initValue);
+        head.setNext(newNode);
+        tail.setPrev(newNode);
+        newNode.setNext(tail);
+        newNode.setPrev(head);
+        size = 1;
+    }
+
+    public void add(int value) {
+        size++;
+        Node newNode = new Node(value);
+        Node curNode = head;
+        while (curNode.getNext() != tail) {
+            curNode = curNode.getNext();
+        }
+        curNode.setNext(newNode);
+        newNode.setPrev(curNode);
+        tail.setPrev(newNode);
+        newNode.setNext(tail);
+    }
+
+    public int remove(int index) {
+        if (size == 0 || (index < 0 && index >= size - 1)) {
+            System.out.println("bad index");
+            return -1;
+        }
+        size--;
+        Node curNode = head.getNext();
+        for (int inc = 0; inc < index; inc++) {
+            curNode = curNode.getNext();
+        }
+        int deleted = curNode.getValue();
+        curNode.getPrev().setNext(curNode.getNext());
+        curNode.getNext().setPrev(curNode.getPrev());
+        return deleted;
+    }
+
+    public int get(int index) {
+        if (size == 0 || (index < 0 && index >= size - 1)) {
+            System.out.println("bad index");
+            return -1;
+        }
+        Node curNode = head.getNext();
+        for (int inc = 0; inc < index; inc++) {
+            curNode = curNode.getNext();
+        }
+        return curNode.getValue();
+    }
 }
