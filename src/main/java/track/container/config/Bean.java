b1d756b1302e2c1bdceb5f8e4980a7ea1d8e281a
Babin Oleg <babinoleg@mail.ru>
2016-10-22 22:53:59 +0300

    lesson-Container

--- src/main/java/track/container/config/Bean.java
+++ src/main/java/track/container/config/Bean.java
@@ -19,6 +19,7 @@ public class Bean {
      */
     private Map<String, Property> properties; // Набор полей бина ИмяПоля-Значение
 
+    public Bean() {}
     public Bean(String id, String className, Map<String, Property> properties) {
         this.id = id;
         this.className = className;
@@ -32,19 +33,15 @@ public class Bean {
     public void setProperties(Map<String, Property> properties) {
         this.properties = properties;
     }
-
     public String getId() {
         return id;
     }
-
     public void setId(String id) {
         this.id = id;
     }
-
     public String getClassName() {
         return className;
     }
-
     public void setClassName(String className) {
         this.className = className;
     }
