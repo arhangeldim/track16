09b89b924a8c8e16b7961cecfbaa80da1d9dcae7
LordOfPhys <acc951995@mail.ru>
2016-11-01 23:29:42 +0300

    First Attempt

--- src/main/java/track/container/Main.java
+++ src/main/java/track/container/Main.java
@@ -1,26 +1,26 @@
 package track.container;
 
-/**
- *
- */
+import track.container.beans.Car;
+import track.container.config.Bean;
+import track.container.config.ConfigReader;
+import track.container.config.InvalidConfigurationException;
+import java.io.File;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        /*
-
-        ПРИМЕР ИСПОЛЬЗОВАНИЯ
-
-         */
-
-//        // При чтении нужно обработать исключение
-//        ConfigReader reader = new JsonReader();
-//        List<Bean> beans = reader.parseBeans("config.json");
-//        Container container = new Container(beans);
-//
-//        Car car = (Car) container.getByClass("track.container.beans.Car");
-//        car = (Car) container.getById("carBean");
-
-
+        ConfigReader reader = new JsonConfigReader();
+        try {
+            File file = new File(JsonConfigReader.class.getClassLoader().getResource("config.json").getFile());
+            List<Bean> bean = reader.parseBeans(file);
+            System.out.println(bean);
+            Container container = new Container(bean);
+            Car car = (Car) container.getByClass("track.container.beans.Car");
+            car = (Car) container.getById("carBean");
+            System.out.println(car);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
