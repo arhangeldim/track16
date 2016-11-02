a190c25bf6eae553d379b6a852ae6ea18bc6cb9f
Leonid Kalichkin <leonid.kalichkin@phystech.edu>
2016-11-02 15:03:44 +0300

    implement field initialization for all primitive and wrapper types

--- src/main/java/track/container/Container.java
+++ src/main/java/track/container/Container.java
@@ -1,5 +1,7 @@
 package track.container;
 
+import java.beans.PropertyEditor;
+import java.beans.PropertyEditorManager;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.LinkedHashMap;
@@ -15,10 +17,10 @@ import track.container.config.ValueType;
  * У него определено 2 публичных метода, можете дописывать свои методы и конструкторы
  */
 public class Container {
-    private Map<String, Bean> beanById;
-    private Map<String, Bean> beanByClass;
-    private Map<String, Object> objById;
-    private Map<String, Object> objByClass;
+    private final Map<String, Bean> beanById;
+    private final Map<String, Bean> beanByClass;
+    private final Map<String, Object> objById;
+    private final Map<String, Object> objByClass;
 
     public Container(List<Bean> beans) {
         beanById = new LinkedHashMap<>();
@@ -69,13 +71,14 @@ public class Container {
     /**
      *  Создать объект на основе бина
      */
+    @SuppressWarnings("unchecked")
     private Object createObjectFromBean(Bean bean) {
         Object object = null;
 
         try {
             /* Найти класс, описанный в бине, и создать объект этого класса */
             Class clazz = Class.forName(bean.getClassName());
-            object = clazz.newInstance();
+            object = clazz.getConstructor().newInstance();
 
             /* Задать маппинги для созданного объекта */
             objById.put(bean.getId(), object);
@@ -83,18 +86,39 @@ public class Container {
 
             /* Десериализовать поля (properties) объекта */
             for (Map.Entry<String, Property> entry : bean.getProperties().entrySet()) {
+
                 Property property = entry.getValue();
 
                 if (property.getType() == ValueType.VAL) {
+
                     /* Если тип поля — значение, то найдём для него сеттер и запишем его в объект через сеттер */
                     try {
-                        /* Пока обрабатываем только int */
-                        Method setter = clazz.getMethod("set" + capitalize(property.getName()), int.class);
-                        setter.invoke(object, Integer.parseInt(property.getValue()));
-                    } catch (NoSuchMethodException | InvocationTargetException e) {
+                        String expectedSetterName = "set" + capitalize(property.getName());
+
+                        /* Найдём сеттер для этого поля */
+                        for (Method setter : clazz.getMethods()) {
+                            if (expectedSetterName.equals(setter.getName())) {
+
+                                /* Определим тип принимаемого параметра */
+                                Class[] parameterTypes = setter.getParameterTypes();
+                                if (parameterTypes.length == 1) {
+                                    Class parameterType = parameterTypes[0];
+
+                                    /*  Преобразуем строковое представление значения поля в примитив или обёртку
+                                        над ним, передадим в сеттер */
+                                    PropertyEditor editor = PropertyEditorManager.findEditor(parameterType);
+                                    editor.setAsText(property.getValue());
+                                    setter.invoke(object, editor.getValue());
+                                }
+
+                                break;
+                            }
+                        }
+                    } catch (InvocationTargetException e) {
                         System.out.println(e.toString());
                     }
                 } else if (property.getType() == ValueType.REF) {
+
                     /* Если тип поля — ссылка, то найдём объект, на который ведёт ссылка */
                     String refId = property.getValue();
                     Object ref = objById.get(refId);
@@ -106,6 +130,7 @@ public class Container {
                         if (refBean != null) {
                             ref = createObjectFromBean(beanById.get(refId));
                         } else {
+
                             /* Если для данной ссылки не описан бин в конфиге, то мы ничего сделать не можем */
                             System.out.println(bean.getId() + " refers to a nonexistent bean " + refId);
                             continue;
@@ -122,7 +147,8 @@ public class Container {
                     }
                 }
             }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                InvocationTargetException | NoSuchMethodException e) {
             System.out.println(e.toString());
         }
 
