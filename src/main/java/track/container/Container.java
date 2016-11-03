752983de27dab373f80a1bab214763bb8e567559
ivan <nagaiko.ivan@gmail.com>
2016-11-03 17:41:57 +0300

    container

--- src/main/java/track/container/Container.java
+++ src/main/java/track/container/Container.java
@@ -1,8 +1,21 @@
 package track.container;
 
+import java.beans.PropertyEditor;
+import java.beans.PropertyEditorManager;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
 import track.container.config.Bean;
+import track.container.config.InvalidConfigurationException;
+import track.container.config.Property;
+import track.container.config.ValueType;
+
+import static org.apache.log4j.config.PropertyPrinter.capitalize;
 
 /**
  * Основной класс контейнера
@@ -10,25 +23,85 @@ import track.container.config.Bean;
  */
 public class Container {
 
+    private Map<String, Bean> beanById;
+    private Map<String, Bean> beanByClassName;
+    private Map<String, Object> objById;
+    private Map<String, Object> objByClassName;
 
     // Реализуйте этот конструктор, используется в тестах!
     public Container(List<Bean> beans) {
-
+        beanById = new HashMap<>();
+        beanByClassName = new HashMap<>();
+        objById = new HashMap<>();
+        objByClassName = new HashMap<>();
+        for (Bean bean : beans) {
+            beanById.put(bean.getId(), bean);
+            beanByClassName.put(bean.getClassName(), bean);
+        }
     }
 
     /**
      *  Вернуть объект по имени бина из конфига
      *  Например, Car car = (Car) container.getById("carBean")
      */
-    public Object getById(String id) {
-        return null;
+    public Object getById(String id) throws InvalidConfigurationException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchFieldException, NoSuchMethodException, InvocationTargetException {
+        Object object = objById.get(id);
+        if (object == null) {
+            Bean bean = beanById.get(id);
+            if (bean != null) {
+                object = createObject(bean);
+            }
+            else {
+                throw new InvalidConfigurationException("Bean without id");
+            }
+        }
+
+        return object;
     }
 
     /**
      * Вернуть объект по имени класса
      * Например, Car car = (Car) container.getByClass("track.container.beans.Car")
      */
-    public Object getByClass(String className) {
-        return null;
+    public Object getByClass(String className) throws InvalidConfigurationException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchFieldException, NoSuchMethodException, InvocationTargetException {
+        Object object = objByClassName.get(className);
+
+        if (object == null) {
+            Bean bean = beanByClassName.get(className);
+            if (bean != null) {
+                object = createObject(bean);
+            }
+            else {
+                throw new InvalidConfigurationException("Bean without class");
+            }
+        }
+
+        return object;
+    }
+
+    private Object createObject(Bean bean) throws ValueException, NoSuchFieldException, NoSuchMethodException, ClassNotFoundException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        Class clazz = Class.forName(bean.getClassName());
+        Object object = clazz.newInstance();
+        objById.put(bean.getId(), object);
+        for (Map.Entry<String, Property> entry : bean.getProperties().entrySet()) {
+            Property property = entry.getValue();
+            Field field = clazz.getDeclaredField(property.getName());
+            Class type = field.getType();
+            Method set = clazz.getMethod(getSetName(property.getName()), type);
+            if (property.getType() == ValueType.VAL) {
+                set.invoke(object, Integer.parseInt(property.getValue()));
+            } else if(property.getType() == ValueType.REF) {
+                if (!objById.containsKey(beanById.get(property.getValue()))) {
+                    createObject(beanById.get(property.getValue()));
+                }
+                set.invoke(object, objById.get(beanById.get(property.getValue())));
+            }
+
+        }
+        return object;
+    }
+
+    private String getSetName(String line) {
+        return "set" + Character.toUpperCase(line.charAt(0)) + line.substring(1);
     }
 }
--- src/main/java/track/container/Main.java
+++ src/main/java/track/container/Main.java
@@ -1,11 +1,25 @@
 package track.container;
 
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
+import track.container.beans.Car;
+import track.container.config.Bean;
+import track.container.config.ConfigReader;
+import track.container.config.InvalidConfigurationException;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.lang.String;
+
+import java.io.File;
+
+import static org.apache.log4j.helpers.Loader.getResource;
+
 /**
  *
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ValueException, NoSuchFieldException, NoSuchMethodException, ClassNotFoundException, InstantiationException, IllegalAccessException, InvocationTargetException, InvalidConfigurationException {
 
         /*
 
@@ -14,12 +28,21 @@ public class Main {
          */
 
 //        // При чтении нужно обработать исключение
-//        ConfigReader reader = new JsonReader();
-//        List<Bean> beans = reader.parseBeans("config.json");
-//        Container container = new Container(beans);
-//
-//        Car car = (Car) container.getByClass("track.container.beans.Car");
-//        car = (Car) container.getById("carBean");
+        File file = new File(getResource("config.json").getFile());
+        ConfigReader reader = new JsonConfigReader();
+        List<Bean> beans;
+        try {
+            beans = reader.parseBeans(file);
+            System.out.print(beans.toString());
+            Container container = new Container(beans);
+            Car car = (Car) container.getByClass("track.container.beans.Car");
+        } catch (InvalidConfigurationException exception) {
+            System.out.println(exception.getMessage());
+        } catch (InstantiationException | InvocationTargetException | IllegalAccessException |
+                NoSuchMethodException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
 
 
     }
