b519609307eacaedab85204a310d913c6d83375a
Vladimir Panov <vladimir.panov@phystech.edu>
2016-11-04 12:45:46 +0300

    make PropertyHelper

--- src/main/java/track/container/Container.java
+++ src/main/java/track/container/Container.java
@@ -13,6 +13,8 @@ import track.container.config.Bean;
 import track.container.config.Property;
 import track.container.config.ValueType;
 
+import static track.container.PropertyHelper.*;
+
 /**
  * Основной класс контейнера
  * У него определено 2 публичных метода, можете дописывать свои методы и конструкторы
@@ -50,8 +52,9 @@ public class Container {
 
     /**
      * Сохраняет объект по bean'у
-     * @param bean      Bean для которого создается соответсвие
-     * @param object    Объект, собранный по этому bean'у
+     *
+     * @param bean   Bean для которого создается соответсвие
+     * @param object Объект, собранный по этому bean'у
      */
     public void saveObject(Bean bean, Object object) {
         objByClassName.put(bean.getClassName(), object);
@@ -60,10 +63,11 @@ public class Container {
 
     /**
      * Обертка над makeInstanceUnwrapped. Перехватывает часть исключений и оборочанивает другими.
-     * @param bean      Бин, по которому создается объект
+     *
+     * @param bean Бин, по которому создается объект
      * @return Object созданный по bean'у
-     * @throws InvocationTargetException       Возникает , если в set[PropertyName] возникает какая-то ошибка
-     * @throws IllegalClassFormatException     Возникает , если в сигнатуре класса допущены ошибки
+     * @throws InvocationTargetException   Возникает , если в set[PropertyName] возникает какая-то ошибка
+     * @throws IllegalClassFormatException Возникает , если в сигнатуре класса допущены ошибки
      */
     private Object makeInstance(Bean bean)
             throws InvocationTargetException,
@@ -88,12 +92,12 @@ public class Container {
     }
 
     /**
-     * @param bean      Bean по которому собирается объект
+     * @param bean Bean по которому собирается объект
      * @return объект , собранный по bean'у
-     * @throws ClassNotFoundException       Ошибка, если класс не найден
-     * @throws IllegalAccessException       Если невозможен доступ к контруктору
-     * @throws InstantiationException       Невозможно создать объект
-     * @throws InvocationTargetException    Ошибка вызова метода set[PropertyName]
+     * @throws ClassNotFoundException    Ошибка, если класс не найден
+     * @throws IllegalAccessException    Если невозможен доступ к контруктору
+     * @throws InstantiationException    Невозможно создать объект
+     * @throws InvocationTargetException Ошибка вызова метода set[PropertyName]
      */
     private Object makeInstanceUnwrapped(Bean bean)
             throws ClassNotFoundException,
@@ -123,66 +127,6 @@ public class Container {
             }
         }
         return obj;
-    }
-
-    private String getSetterMethodName(String propertyName) {
-        return "set" + propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1);
-    }
-
-    /**
-     * Нужен для определения типа параметра для set[PropertyName] метода.
-     *
-     * @param clazz    класс, для которого ищется тип свойства
-     * @param property свойство
-     * @return Class    тип свойства
-     * @throws NoSuchFieldException     Если поле не было найдено
-     * @throws IllegalAccessException   Нет доступа к полю класса
-     */
-    private Class getValSetterMethodParamType(Class clazz, Property property)
-            throws NoSuchFieldException, IllegalAccessException {
-        Field field = clazz.getDeclaredField(property.getName());
-        Class type = field.getType();
-        if (type.equals(long.class)) {
-            return long.class;
-        } else if (type.equals(int.class)) {
-            return int.class;
-        } else if (type.equals(short.class)) {
-            return short.class;
-        } else if (type.equals(byte.class)) {
-            return byte.class;
-        } else if (type.equals(double.class)) {
-            return double.class;
-        } else if (type.equals(float.class)) {
-            return float.class;
-        } else if (type.equals(boolean.class)) {
-            return boolean.class;
-        }
-        throw new IllegalArgumentException("Property type is not primitive type");
-    }
-
-    /**
-     * Преобразует строку значения поля некоторого примитивного типа в нужный тип
-     * @param type              необходимое значение типа поля
-     * @param propertyValue     строковое значение этого поля
-     * @return объект , преобразованный в нужный тип данных
-     */
-    private Object getValSetterMethodParam(Class type, String propertyValue) {
-        if (type.equals(long.class)) {
-            return Long.parseLong(propertyValue);
-        } else if (type.equals(int.class)) {
-            return Integer.parseInt(propertyValue);
-        } else if (type.equals(short.class)) {
-            return Short.parseShort(propertyValue);
-        } else if (type.equals(byte.class)) {
-            return Byte.parseByte(propertyValue);
-        } else if (type.equals(double.class)) {
-            return Double.parseDouble(propertyValue);
-        } else if (type.equals(float.class)) {
-            return Float.parseFloat(propertyValue);
-        } else if (type.equals(boolean.class)) {
-            return Boolean.parseBoolean(propertyValue);
-        }
-        throw new IllegalArgumentException("Property type is not primitive type");
     }
 
     /**
