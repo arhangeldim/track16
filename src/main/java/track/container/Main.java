a33e90da2e6812a03994b079c576f06207d05067
Malik <gazizullin.malik2010@yandex.ru>
2016-11-10 17:50:58 +0300

    g

--- src/main/java/track/container/Main.java
+++ src/main/java/track/container/Main.java
@@ -1,7 +1,10 @@
 package track.container;
 
+import track.container.beans.Car;
+import track.container.beans.Gear;
 import track.container.config.InvalidConfigurationException;
 
+import javax.naming.ConfigurationException;
 import java.io.IOException;
 
 /**
@@ -9,26 +12,26 @@ import java.io.IOException;
  */
 public class Main {
 
-    public static void main(String[] args) throws InvalidConfigurationException, IOException, ClassNotFoundException {
+    public static void main(String[] args) throws InvalidConfigurationException, IOException, ClassNotFoundException, ConfigurationException {
 
-/*        Container container = new Container("/home/user/Documents/Git/track16/src/main/resources/config.json");
-        Car car = (Car) container.getByClass("track.container.beans.Car");
-        System.out.println(car);
+//        Container container = new Container("/home/user/Documents/Git/track16/src/main/resources/config.json");
+//        Car car = (Car) container.getByClass("track.container.beans.Car");
+//        System.out.println(car);
+//
+//        //Container cont = new Container("/home/user/Documents/Git/track16/src/main/resources/config.json");
+//        Gear gear = (Gear) container.getById("gearBean");
+//        System.out.println(gear);
 
-        //Container cont = new Container("/home/user/Documents/Git/track16/src/main/resources/config.json");
-        Gear gear = (Gear) container.getById("gearBean");
-        System.out.println(gear);*/
-        /*
 
-        ПРИМЕР ИСПОЛЬЗОВАНИЯ
-
-         */
+//        ПРИМЕР ИСПОЛЬЗОВАНИЯ
+//
+//         */
 
 //        // При чтении нужно обработать исключение
 //        ConfigReader reader = new JsonReader();
 //        List<Bean> beans = reader.parseBeans("config.json");
 //        Container container = new Container(beans);
-//
+////
 //        Car car = (Car) container.getByClass("track.container.beans.Car");
 //        car = (Car) container.getById("carBean");
 
