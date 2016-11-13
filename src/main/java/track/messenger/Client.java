b2a679db960e41f5aed48202e84d4cdf08c55556
George Komarov <geoolekom@gmail.com>
2016-11-13 22:12:26 +0300

    Messenger basis

---
+++ src/main/java/track/messenger/Client.java
@@ -0,0 +1,7 @@
+package track.messenger;
+
+/**
+ * Created by geoolekom on 13.11.16.
+ */
+public class Client {
+}
---
+++ src/main/java/track/messenger/messages/InfoMessage.java
@@ -0,0 +1,7 @@
+package track.messenger.messages;
+
+/**
+ * Created by geoolekom on 13.11.16.
+ */
+public class InfoMessage {
+}
