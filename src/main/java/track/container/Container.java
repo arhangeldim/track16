9e635f1c156abb386fb7978eadb10773422fb618
lesina <lesha_lesovoy@mail.ru>
2016-11-14 17:19:31 +0300

    Added function setField and added case to string

--- src/main/java/track/container/Container.java
+++ src/main/java/track/container/Container.java
@@ -48,36 +48,7 @@ public class Container {
                 for (Field field : fields) {
                     if (property.equals(field.getName())) {
                         if (bean.getProperties().get(property).getType() == ValueType.VAL) {
-                            field.setAccessible(true);
-                            String value = bean.getProperties().get(field.getName()).getValue();
-                            switch (field.getType().getTypeName().toLowerCase()) {
-                                case "boolean":
-                                    field.set(object, Boolean.parseBoolean(value));
-                                    break;
-                                case "byte":
-                                    field.set(object, Byte.parseByte(value));
-                                    break;
-                                case "short":
-                                    field.set(object, Short.parseShort(value));
-                                    break;
-                                case "int":
-                                    field.set(object, Integer.parseInt(value));
-                                    break;
-                                case "char":
-                                    field.set(object, value.charAt(0));
-                                    break;
-                                case "long":
-                                    field.set(object, Long.parseLong(value));
-                                    break;
-                                case "float":
-                                    field.set(object, Float.parseFloat(value));
-                                    break;
-                                case "double":
-                                    field.set(object, Double.parseDouble(value));
-                                    break;
-                                default:
-                                    throw new ClassNotFoundException("No such primitive type " + field.getType().getTypeName().toLowerCase());
-                            }
+                            setField(field, bean, object);
                         } else {
                             String subName = findBean(bean.getProperties().get(property).getValue()).getClassName();
                             Class subClazz = Class.forName(subName);
@@ -110,6 +81,42 @@ public class Container {
             }
         }
         throw new InvalidConfigurationException("Invalid class name");
+    }
+
+    private void setField(Field field, Bean bean, Object object) throws ClassNotFoundException, IllegalAccessException{
+        field.setAccessible(true);
+        String value = bean.getProperties().get(field.getName()).getValue();
+        switch (field.getType().getTypeName().toLowerCase()) {
+            case "boolean":
+                field.set(object, Boolean.parseBoolean(value));
+                break;
+            case "byte":
+                field.set(object, Byte.parseByte(value));
+                break;
+            case "short":
+                field.set(object, Short.parseShort(value));
+                break;
+            case "int":
+                field.set(object, Integer.parseInt(value));
+                break;
+            case "char":
+                field.set(object, value.charAt(0));
+                break;
+            case "long":
+                field.set(object, Long.parseLong(value));
+                break;
+            case "float":
+                field.set(object, Float.parseFloat(value));
+                break;
+            case "double":
+                field.set(object, Double.parseDouble(value));
+                break;
+            case "string":
+                field.set(object, value);
+                break;
+            default:
+                throw new ClassNotFoundException("No such primitive type " + field.getType().getTypeName().toLowerCase());
+        }
     }
 
     private Bean findBean(String id) throws InvalidConfigurationException {
