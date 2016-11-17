c4d3c2102781b71100fbd7b374c10ed81edc06ec
George Komarov <geoolekom@gmail.com>
2016-11-17 19:23:29 +0300

    Chat list added

---
+++ src/main/java/track/messenger/commands/ChatListCommand.java
@@ -0,0 +1,7 @@
+package track.messenger.commands;
+
+/**
+ * Created by geoolekom on 17.11.16.
+ */
+public class ChatListCommand {
+}
---
+++ src/main/java/track/messenger/messages/ChatListResultMessage.java
@@ -0,0 +1,7 @@
+package track.messenger.messages;
+
+/**
+ * Created by geoolekom on 17.11.16.
+ */
+public class ChatListResultMessage {
+}
