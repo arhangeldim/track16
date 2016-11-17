7cd40151322e008306dc316afeb208c751c51209
Rishat <fazlytdinovrishat@gmail.com>
2016-11-17 12:43:51 +0300

    Changed according to comments

--- src/main/java/track/container/Container.java
+++ src/main/java/track/container/Container.java
@@ -1,14 +1,12 @@
 package track.container;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import track.container.config.Bean;
+import track.container.config.InvalidConfigurationException;
 import track.container.config.Property;
 import track.container.config.ValueType;
 
@@ -39,7 +37,13 @@ public class Container {
      *  Например, Car car = (Car) container.getById("carBean")
      */
     public Object getById(String id) {
-        return makeRequiredObject(beansById.get(id));
+        try {
+            return makeRequiredObject(beansById.get(id));
+        }
+        catch (InvalidConfigurationException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 
     /**
@@ -47,14 +51,20 @@ public class Container {
      * Например, Car car = (Car) container.getByClass("track.container.beans.Car")
      */
     public Object getByClass(String className) {
-        return makeRequiredObject(beansByClassName.get(className));
+        try {
+            return makeRequiredObject(beansByClassName.get(className));
+        }
+        catch (InvalidConfigurationException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 
-    public Object makeRequiredObject(Bean bean) {
+    public Object makeRequiredObject(Bean bean) throws InvalidConfigurationException {
         try {
             Class beanClass = Class.forName(bean.getClassName());
             Object object = beanClass.getConstructor().newInstance();
-            for (Property property: bean.getProperties()) {
+            for (Property property : bean.getProperties()) {
                 Object propertyValue = null;
                 if (property.getType().equals(ValueType.VAL)) {
                     propertyValue = property.getVal();
@@ -65,21 +75,98 @@ public class Container {
                 char[] properyNameCharSet = property.getName().toCharArray();
                 properyNameCharSet[0] = Character.toUpperCase(properyNameCharSet[0]);
                 String properyName = new String(properyNameCharSet);
-                for (Method method: object.getClass().getDeclaredMethods()) {
+                for (Method method : object.getClass().getDeclaredMethods()) {
                     if (method.getName().equals("set" + properyName)) {
-                        if (method.getParameterTypes()[0].getName().equals(INT)) {
-                            propertyValue = Integer.parseInt((String) propertyValue);
+                        for (Primitives primitive: Primitives.values()){
+                            if (method.getParameterTypes()[0].getName().equals(primitive.getType())){
+                                propertyValue = primitive.parse((String) propertyValue);
+                            }
                         }
-                        method.invoke(object,propertyValue);
+                        method.invoke(object, propertyValue);
                     }
                 }
             }
             return object;
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        return null;
+        catch (Exception e){
+            InvalidConfigurationException ex = new InvalidConfigurationException("Invalid config");
+            throw ex;
+        }
+    }
+
+
+    private enum Primitives{
+        BOOLEAN{
+            private String type = "boolean";
+            public Boolean parse(String value){
+                return Boolean.parseBoolean(value);
+            }
+
+            @Override
+            public String getType() {
+                return type;
+            }
+        },
+        SHORT{
+            private String type = "short";
+            public Short parse(String value){
+                return Short.parseShort(value);
+            }
+
+            @Override
+            public String getType() {
+                return type;
+            }
+        },
+        INTEGER{
+            private String type = "int";
+            public Integer parse(String value){
+                return Integer.parseInt(value);
+            }
+
+            @Override
+            public String getType() {
+                return type;
+            }
+        },
+        LONG{
+            private String type = "long";
+            public Long parse(String value){
+                return Long.parseLong(value);
+            }
+
+            @Override
+            public String getType() {
+                return type;
+            }
+
+        },
+        DOUBLE{
+            private String type = "double";
+            public Double parse(String value){
+                return Double.parseDouble(value);
+            }
+
+            @Override
+            public String getType() {
+                return type;
+            }
+        },
+        FLOAT{
+            private String type = "float";
+            public Float parse(String value){
+                return Float.parseFloat(value);
+            }
+
+            @Override
+            public String getType() {
+                return type;
+            }
+        };
+
+        public abstract Object parse(String value);
+        public abstract String getType();
+
     }
 
 }
